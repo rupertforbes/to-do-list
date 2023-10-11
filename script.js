739f8dac5fab2c985f9a43be061c020996381a05
rupertforbes <127436772+rupertforbes@users.noreply.github.com>
2023-10-11 15:40:32 +0100

    Added basic functionality to input in JS

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function showItem(event) {
   event.preventDefault();
-  console.log("Hello");
+  let newItem = inputForm.value;
+  target.innerHTML = `<li>${newItem}</li>`;
 }
 
+let inputForm = document.querySelector(".input");
 let searchForm = document.querySelector(".search-form");
 let submitButton = document.querySelector(".done");
+let target = document.querySelector(".target");
 searchForm.addEventListener("submit", showItem);
 submitButton.addEventListener("click", showItem);
